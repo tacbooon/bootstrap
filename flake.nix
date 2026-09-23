26ed834f7075b54aef9ddef7388320e4dc525906
tacbooon <tacbooon@icloud.com>
2026-09-23 15:09:33 +0900

    devShell を導入

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Dev shell for bootstrap";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.actionlint
+              pkgs.nixfmt
+              pkgs.shellcheck
+              pkgs.zizmor
+            ];
+          };
+        }
+      );
+    };
+}
